11bb684487d0db4b94ab6f50937a9c2d60050568
PooYooGitHub <ysy3083446265@gmail.com>
2024-07-26 20:13:58 +0800

    complete basic son module

---
+++ admin/src/main/java/com/project/AdminApplication.java
@@ -0,0 +1,25 @@
+package com.project;/**
+ * author:
+ * data:2024/7/26 下午7:59
+ * function:
+ * time:
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: shortlink
+ * @package: com.project
+ * @className: adminApplication
+ * @author: Eric
+ * @description:
+ * @date: 2024/7/26 下午7:59
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
