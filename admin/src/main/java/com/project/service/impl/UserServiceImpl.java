ab04d72ca08a9ba31e7aea5ad44974c26575adfa
PooYooGitHub <ysy3083446265@gmail.com>
2024-07-31 10:04:25 +0800

    complete GlobalExceptionHandler

--- admin/src/main/java/com/project/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/project/service/impl/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.project.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.project.common.convention.exception.ClientException;
+import com.project.common.enums.UserErrorCode;
 import com.project.dao.entity.UserDO;
 import com.project.dao.mapper.UserMapper;
 import com.project.dto.resp.UserRespDTO;
@@ -23,7 +25,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
                 .eq(UserDO::getUsername, username);
         UserDO userDO = baseMapper.selectOne(eq);
         if (userDO == null) {
-            return null;
+            throw new ClientException(UserErrorCode.USER_NUll);
         }
         UserRespDTO result = new UserRespDTO();
         BeanUtils.copyProperties(userDO, result);
