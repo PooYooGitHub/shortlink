eb42435a0a22e56b59c68a74377ff671141bbfc7
PooYooGitHub <ysy3083446265@gmail.com>
2024-08-16 23:59:47 +0800

    programme: 使用布隆过滤器完善添加短链接功能,解决不同原始链接产生相同uri的问题

--- programe/src/main/java/com/project/service/impl/ShortLinkServiceImpl.java
+++ programe/src/main/java/com/project/service/impl/ShortLinkServiceImpl.java
@@ -2,31 +2,41 @@ package com.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.project.common.convention.exception.ClientException;
 import com.project.dao.entity.ShortLinkDO;
 import com.project.dao.mapper.ShortLinkMapper;
 import com.project.dto.req.ShortLinkCreateReqDTO;
 import com.project.dto.resp.ShortLinkCreateRespDTO;
 import com.project.service.ShortLinkService;
 import com.project.util.HashUtil;
+import lombok.AllArgsConstructor;
+import org.redisson.api.RBloomFilter;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements ShortLinkService {
+    private final RBloomFilter<String> shortLinkCachePenetrationBloomFilter;
 
     @Override
     public ShortLinkCreateRespDTO create(ShortLinkCreateReqDTO requestParam) {
-        String shortUri = HashUtil.hashToBase62(requestParam.getOriginUrl());
+
+        //TODO: 生成短链接使用的是hash算法，可能会存在hash冲突，需要处理
+        //将 新生成的url存入布隆过滤器,好像可以解决
+
+        String shortUri ;
+        String shortUrl ;
+        do {
+            shortUri = HashUtil.hashToBase62(requestParam.getOriginUrl()+System.currentTimeMillis());
+            shortUrl = requestParam.getDomain() + "/" + shortUri;
+        }while (shortLinkCachePenetrationBloomFilter.contains(shortUrl));
 
         ShortLinkDO shortLinkDO = BeanUtil.toBean(requestParam, ShortLinkDO.class);
         shortLinkDO.setShortUri(shortUri);
-        shortLinkDO.setFullShortUrl(requestParam.getDomain() + "/" + shortUri);
+        shortLinkDO.setFullShortUrl(shortUrl);
 
-        try {
-            baseMapper.insert(shortLinkDO);
-        } catch (Exception e) {
-            throw new ClientException("短链接已经存在,请勿重复创建");
-        }
+        baseMapper.insert(shortLinkDO);
+        shortLinkCachePenetrationBloomFilter.add(shortUrl);
+
 
         return ShortLinkCreateRespDTO
                 .builder().
@@ -35,5 +45,6 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 fullShortUrl(shortLinkDO.getFullShortUrl())
                 .build();
     }
+
 
 }
