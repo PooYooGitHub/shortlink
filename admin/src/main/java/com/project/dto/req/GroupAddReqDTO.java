2667258fafb44594c6fa7bf507f7310fef4838bd
PooYooGitHub <ysy3083446265@gmail.com>
2024-08-20 15:19:50 +0800

    admin: 修改属性名

--- admin/src/main/java/com/project/dto/req/GroupAddReqDTO.java
+++ admin/src/main/java/com/project/dto/req/GroupAddReqDTO.java
@@ -1,11 +1,15 @@
 package com.project.dto.req;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GroupAddReqDTO {
     /**
      * 词条名
      */
-    public String name;
+    public String groupName;
 }
