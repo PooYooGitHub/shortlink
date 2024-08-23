51a3584e9efcec6a6d4beaf5db8f227d259da9b3
PooYooGitHub <ysy3083446265@gmail.com>
2024-08-23 09:47:01 +0800

    shortlink: 完成缓存过期时间生成实体

---
+++ programe/src/main/java/com/project/util/LinkUtil.java
@@ -0,0 +1,44 @@
+package com.project.util;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 短链接工具类
+ */
+
+public class LinkUtil {
+    public static final long ONE_WEEK_MILLIS = 7 * 24 * 60 * 60 * 1000L;
+
+    /**
+     * 获取短链接缓存过期时间戳
+     *
+     * @param date 日期
+     * @return 短链接缓存过期时间戳
+     */
+    public static long getShortLinkCacheTime(Date date) {
+        // 当前时间
+        long currentTimeMillis = System.currentTimeMillis();
+
+        // 如果 date 为 null（永久有效），则默认设置为一周后的时间戳，并随机一个 0 ~ 24 小时的时间
+        if (date == null) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DAY_OF_MONTH, 7);
+            return calendar.getTimeInMillis() + ThreadLocalRandom.current().nextLong(0, 24 * 60 * 60 * 1000L)-currentTimeMillis;
+        }
+
+        long dateMillis = date.getTime();
+
+        // 检查 date 是否超过一周
+        if (dateMillis - currentTimeMillis > ONE_WEEK_MILLIS) {
+            // 如果超过一周，那么将有效时间设置在一周后
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DAY_OF_MONTH, 7);
+            return calendar.getTimeInMillis() + ThreadLocalRandom.current().nextLong(0, 24 * 60 * 60 * 1000L)-currentTimeMillis;
+        } else {
+            // 如果没有超过一周，则设置为 date 的时间戳
+            return dateMillis-currentTimeMillis;
+        }
+    }
+}
