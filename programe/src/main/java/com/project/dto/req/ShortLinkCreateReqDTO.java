228121d6606aa29e6042fb6666482e506f761088
PooYooGitHub <ysy3083446265@gmail.com>
2024-08-23 09:46:18 +0800

    shortlink: 修改实体属性类型

--- programe/src/main/java/com/project/dto/req/ShortLinkCreateReqDTO.java
+++ programe/src/main/java/com/project/dto/req/ShortLinkCreateReqDTO.java
@@ -3,6 +3,8 @@ package com.project.dto.req;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * 短链接创建请求实体
  */
@@ -11,7 +13,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 创建类型，0：接口创建 1：控制台创建
      */
-    private long createdType;
+    private Integer createdType;
     /**
      * 描述
      */
@@ -32,9 +34,9 @@ public class ShortLinkCreateReqDTO {
      * 有效期
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    private String validDate;
+    private Date validDate;
     /**
      * 有效期类型，0：永久有效 1：自定义
      */
-    private long validDateType;
+    private Integer validDateType;
 }
