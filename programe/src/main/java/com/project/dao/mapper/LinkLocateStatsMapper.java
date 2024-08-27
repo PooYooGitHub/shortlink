9ba2466d4c513f1d9f1c1445a93436ee525b47da
PooYooGitHub <ysy3083446265@gmail.com>
2024-08-27 23:52:40 +0800

    shortlink: 完成添加访问网址用户属地功能

--- programe/src/main/java/com/project/dao/mapper/LinkLocateStatsMapper.java
+++ programe/src/main/java/com/project/dao/mapper/LinkLocateStatsMapper.java
@@ -2,7 +2,40 @@ package com.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.project.dao.entity.LinkLocateStatsDO;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface LinkLocateStatsMapper extends BaseMapper<LinkLocateStatsDO> {
+    @Insert("""
+            INSERT INTO t_link_locate_stats (
+              full_short_url,
+              gid,
+              date,
+              cnt,
+                province,
+                city,
+                adcode,
+                country,
+              create_time,
+              update_time,
+              del_flag
+            ) VALUES (
+              #{linkLocateStatsDO.fullShortUrl},
+              #{linkLocateStatsDO.gid},
+              #{linkLocateStatsDO.date},
+              #{linkLocateStatsDO.cnt},
+              #{linkLocateStatsDO.province},
+              #{linkLocateStatsDO.city},
+              #{linkLocateStatsDO.adcode},
+              #{linkLocateStatsDO.country},
+              NOW(),
+              NOW(),
+              #{linkLocateStatsDO.delFlag}
+            ) ON DUPLICATE KEY UPDATE
+              cnt = #{linkLocateStatsDO.cnt} + cnt
+              ;""")
+    void insertOrUpdate(@Param("linkLocateStatsDO") LinkLocateStatsDO linkLocateStatsDO);
 
 }
--- programe/src/main/java/com/project/dao/mapper/ShortLinkGoToMapper.java
+++ programe/src/main/java/com/project/dao/mapper/ShortLinkGoToMapper.java
@@ -2,6 +2,8 @@ package com.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.project.dao.entity.ShortLinkGoToDO;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface ShortLinkGoToMapper extends BaseMapper<ShortLinkGoToDO> {
 }
