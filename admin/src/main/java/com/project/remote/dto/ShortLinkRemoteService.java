4aa080bbb3e6ab676afcc6ac25ee2f110fe33351
PooYooGitHub <ysy3083446265@gmail.com>
2024-08-28 23:20:33 +0800

    短链接统计：完成远程调用相关代码

--- admin/src/main/java/com/project/remote/dto/ShortLinkRemoteService.java
+++ admin/src/main/java/com/project/remote/dto/ShortLinkRemoteService.java
@@ -1,5 +1,6 @@
 package com.project.remote.dto;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 import com.alibaba.fastjson2.JSON;
@@ -8,9 +9,11 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.project.common.convention.result.Result;
 import com.project.remote.dto.req.ShortLinkCreateReqDTO;
 import com.project.remote.dto.req.ShortLinkPageReqDTO;
+import com.project.remote.dto.req.ShortLinkStatsReqDTO;
 import com.project.remote.dto.resp.GroupShortLinkCountRespDTO;
 import com.project.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.project.remote.dto.resp.ShortLinkPageRespDTO;
+import com.project.remote.dto.resp.ShortLinkStatsRespDTO;
 
 import java.util.HashMap;
 import java.util.List;
@@ -59,6 +62,17 @@ public interface ShortLinkRemoteService {
         hashMap.put("gid", gids);
         String resp = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/count", hashMap);
         return JSON.parseObject(resp, new TypeReference<Result<List<GroupShortLinkCountRespDTO>>>() {
+        });
+    }
+    /**
+     * 访问单个短链接指定时间内监控数据
+     *
+     * @param requestParam 访问短链接监控请求参数
+     * @return 短链接监控信息
+     */
+    default Result<ShortLinkStatsRespDTO> oneShortLinkStats(ShortLinkStatsReqDTO requestParam) {
+        String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats", BeanUtil.beanToMap(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
         });
     }
 
